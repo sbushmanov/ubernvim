9b59fcae22cdf2b0e3e081409e2ba4481888876b
sergey <sergey.bushmanov@gmail.com>
2022-12-12 20:56:20 +0300

    alpha

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -9,23 +9,12 @@ function M.setup()
   local dashboard = require "alpha.themes.dashboard"
   local function header()
     return {
-      [[                                           bbbbbbbb            ]],
-      [[                                           b::::::b            ]],
-      [[                                           b::::::b            ]],
-      [[                                           b::::::b            ]],
-      [[                                            b:::::b            ]],
-      [[nnnn  nnnnnnnn    vvvvvvv           vvvvvvv b:::::bbbbbbbbb    ]],
-      [[n:::nn::::::::nn   v:::::v         v:::::v  b::::::::::::::bb  ]],
-      [[n::::::::::::::nn   v:::::v       v:::::v   b::::::::::::::::b ]],
-      [[nn:::::::::::::::n   v:::::v     v:::::v    b:::::bbbbb:::::::b]],
-      [[  n:::::nnnn:::::n    v:::::v   v:::::v     b:::::b    b::::::b]],
-      [[  n::::n    n::::n     v:::::v v:::::v      b:::::b     b:::::b]],
-      [[  n::::n    n::::n      v:::::v:::::v       b:::::b     b:::::b]],
-      [[  n::::n    n::::n       v:::::::::v        b:::::b     b:::::b]],
-      [[  n::::n    n::::n        v:::::::v         b:::::bbbbbb::::::b]],
-      [[  n::::n    n::::n         v:::::v          b::::::::::::::::b ]],
-      [[  n::::n    n::::n          v:::v           b:::::::::::::::b  ]],
-      [[  nnnnnn    nnnnnn           vvv            bbbbbbbbbbbbbbbb   ]],
+[[   _____            __     ]],
+[[  / ___/_________ _/ /___ _]],
+[[  \__ \/ ___/ __ `/ / __ `/]],
+[[ ___/ / /__/ /_/ / / /_/ / ]],
+[[/____/\___/\__,_/_/\__,_/  ]],
+[[                           ]],
     }
   end
 
